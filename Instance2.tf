154cdf667812b8e89a457c151868fe0de1f23907
nikhilpnr <57071029+nikhilpnr@users.noreply.github.com>
2022-06-18 11:09:55 +0530

    Update Instance2.tf

--- Instance2.tf
+++ Instance2.tf
@@ -16,10 +16,10 @@ resource "aws_instance" "example" {
   count         = "1"
   user_data     = <<EOF
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
+apt-get update
+apt-get install -y apache2
+systemctl start apache2
+systemctl enable apache2
 echo "<h1> Hello bosch</h1>" | sudo tee /var/www/html/index.html
 EOF
   tags = {
