132f460faba0c7b210faf92b49eb0ad211ef40b8
nikhilpnr <57071029+nikhilpnr@users.noreply.github.com>
2022-06-21 15:57:01 +0530

    Update Instance1.tf

--- Instance1.tf
+++ Instance1.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region  = "ap-south-1"
+  region  = "eu-central-1"
 }
  
 resource "aws_instance" "ec2-web" {
-  ami           = "ami-079b5e5b3971bd10d"
+  ami           = "ami-0a1ee2fb28fe05df3"
   instance_type = "t2.micro"
  
   tags = {
