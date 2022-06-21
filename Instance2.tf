1de982e7372326cc7054e2843b2549f4a98e6685
nikhilpnr <57071029+nikhilpnr@users.noreply.github.com>
2022-06-21 19:11:06 +0530

    Update Instance2.tf

--- Instance2.tf
+++ Instance2.tf
@@ -8,20 +8,13 @@ terraform {
 }
  
 provider "aws" {
-  region = "ap-south-1"
+  region = "eu-central-q"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-068257025f72f470d"
+  ami           = "ami-0a1ee2fb28fe05df3"
   instance_type = "t2.micro"
   count         = "1"
-  user_data     = <<EOF
-#!/bin/bash
-apt-get update
-apt-get install -y apache2
-systemctl start apache2
-systemctl enable apache2
-echo "<h1> Hello bosch</h1>" | sudo tee /var/www/html/index.html
-EOF
+  user_data     = file("./script.sh")
   tags = {
     Name = "terraform-example"
   }
