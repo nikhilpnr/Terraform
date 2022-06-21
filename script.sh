f66dcb4b71403affb595e366ad1c6abf2b7076bd
nikhilpnr <57071029+nikhilpnr@users.noreply.github.com>
2022-06-21 19:12:16 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@
   sudo systemctl start httpd
   chkconfig httpd on
   cd /var/www/html
-  echo "<html><h1>Hello Team Welcome To My Webpage</h1></html>" > index.html
+  echo "<html><h1>Hello BSH</h1></html>" > index.html
