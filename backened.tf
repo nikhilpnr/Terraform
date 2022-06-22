dc5c73f744db2d7d20b40da17f194ca70e558c73
nikhilpnr <57071029+nikhilpnr@users.noreply.github.com>
2022-06-22 12:17:17 +0530

    Create backened.tf

---
+++ backened.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "tg-bshn-state"
+    key            = "backend/terraform.tfstate"
+    region         = "eu-central-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "tg-bosch-dynamo"
+    encrypt        = true
+  }
+}
